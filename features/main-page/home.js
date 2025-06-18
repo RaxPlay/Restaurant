10a06705289346eabe48004c1e1791c9175d8731
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-06-17 19:53:56 -0700

    99% Finished Almost done with the project, I just need to add some little technical and visual details.

--- features/main-page/home.js
+++ features/main-page/home.js
@@ -1,3 +1,34 @@
-//variables
+//Variables
+let menuBtn = document.querySelector(".menu-component");
+let contactBtn = document.querySelector(".contact-component");
+let aboutBtn = document.querySelector(".about-component");
 
-//functions
+//Functions
+function goHome(){
+    window.scrollTo({
+        top: 0,
+
+        behavior: "smooth"
+    });
+};
+menuBtn.addEventListener('click', function(){
+    window.scrollTo({
+        top: 917,
+
+        behavior: "smooth"
+    });
+});
+contactBtn.addEventListener('click', function(){
+    window.scrollTo({
+        top:  1500,
+
+        behavior: "smooth"
+    });
+});
+aboutBtn.addEventListener('click', function(){
+    window.scrollTo({
+        top: 1900,
+
+        behavior: "smooth"
+    });
+});
